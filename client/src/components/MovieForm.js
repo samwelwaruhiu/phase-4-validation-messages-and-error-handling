537f533984773622248f78fcf1873d74b4060ebc
Samwel Waruhiu <samweliwaruhiu@gmail.com>
2022-11-24 12:38:20 +0300

    done

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -14,6 +14,8 @@ function MovieForm() {
     female_director: false,
   });
 
+  const [errors, setErrors] = useState([]);
+
   function handleSubmit(e) {
     e.preventDefault();
     fetch("/movies", {
@@ -23,7 +25,13 @@ function MovieForm() {
       },
       body: JSON.stringify(formData),
     })
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.ok) {
+          response.json().then((newMovie) => console.log(newMovie));
+        } else {
+          response.json().then((errorData) => setErrors(errorData.errors));
+        }
+      })
       .then((newMovie) => console.log(newMovie));
   }
 
@@ -125,6 +133,13 @@ function MovieForm() {
             />
           </label>
         </FormGroup>
+        {errors.length > 0 && (
+    <ul style={{ color: "red" }}>
+      {errors.map((error) => (
+        <li key={error}>{error}</li>
+      ))}
+    </ul>
+  )}
         <SubmitButton type="submit">Add Movie</SubmitButton>
       </form>
     </Wrapper>
